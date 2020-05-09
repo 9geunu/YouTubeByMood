e5c177095dc653ff49fa504ed92eef8eccec712f
walter <dev.ggw@gmail.com>
2020-05-09 21:21:24 +0900

    WeatherSyncTask 패키지명 변경 sync

--- app/src/main/java/com/example/youtubebymood/core/WeatherSyncTask.java
+++
@@ -1,4 +0,0 @@
-package com.example.youtubebymood.core;
-
-public class WeatherSyncTask {
-}
---
+++ app/src/main/java/com/example/youtubebymood/sync/WeatherSyncTask.java
@@ -0,0 +1,27 @@
+package com.example.youtubebymood.core;
+
+import android.content.Context;
+
+import com.example.youtubebymood.utils.NetworkUtils;
+import com.example.youtubebymood.utils.OpenWeatherMapJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class WeatherSyncTask {
+    synchronized public static void syncWeather(Context context){
+
+        try {
+            URL weatherRequestUrl = NetworkUtils.getUrl(context);
+
+            String jsonWeatherResponse = NetworkUtils.getResponseFromHttpUrl(weatherRequestUrl);
+
+            OpenWeatherMapJsonUtils.setCurrentWeatherModelFromJson(context, jsonWeatherResponse);
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
