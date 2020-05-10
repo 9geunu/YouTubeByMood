a1762f8e5b24dc14c3ccec8e802640651ae96e99
walter <dev.ggw@gmail.com>
2020-05-10 19:42:58 +0900

    Search 구현

--- app/src/main/java/com/example/youtubebymood/core/Search.java
+++ app/src/main/java/com/example/youtubebymood/core/Search.java
@@ -2,6 +2,7 @@ package com.example.youtubebymood.core;
 
 import android.util.Log;
 
+import com.example.youtubebymood.core.model.YoutubeVideoModel;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
@@ -13,7 +14,6 @@ import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
-import com.google.api.services.youtube.model.Thumbnail;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoListResponse;
 
@@ -32,14 +32,14 @@ public class Search {
     private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
     private static YouTube youtube;
 
-    public static  ArrayList<YoutubeVideoModel> getYoutubeVideoList() {
-        Log.d("log", "main start");
+    public static  ArrayList<YoutubeVideoModel> getYoutubeVideoList(String query) {
+        Log.d(Constants.LOGTAG, "search start");
         try {
             youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
                 public void initialize(HttpRequest request) {}
             }).setApplicationName("youtube-by-mood").build();
 
-            String queryTerm = getInputQuery("비 오는 날 노래");
+            String queryTerm = getInputQuery(query);
 
             YouTube.Search.List search = youtube.search().list("id,snippet");
 
@@ -50,21 +50,23 @@ public class Search {
              * This method reduces the info returned to only the fields we need and makes calls more
              * efficient.
              */
-            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/publishedAt,snippet/channelTitle)");
             search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
             SearchListResponse searchResponse = search.execute();
 
             List<SearchResult> searchResultList = searchResponse.getItems();
 
+            Log.d(Constants.LOGTAG, "Search Finished!");
+
             if (searchResultList != null) {
                 return createYouTubeList(searchResultList.iterator(), queryTerm);
             }
         } catch (GoogleJsonResponseException e) {
-            Log.d("log","There was a service error: " + e.getDetails().getCode() + " : "
+            Log.d(Constants.LOGTAG,"There was a service error: " + e.getDetails().getCode() + " : "
                     + e.getDetails().getMessage());
             return null;
         } catch (IOException e) {
-            Log.d("log","There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            Log.d(Constants.LOGTAG,"There was an IO error: " + e.getCause() + " : " + e.getMessage());
             return null;
         } catch (Throwable t) {
             t.printStackTrace();
@@ -73,7 +75,7 @@ public class Search {
         return null;
     }
 
-    private static String getInputQuery(String inputQuery) throws IOException {
+    private static String getInputQuery(String inputQuery) {
         inputQuery.replace("","+");
 
         return inputQuery;
@@ -81,15 +83,8 @@ public class Search {
 
     private static ArrayList<YoutubeVideoModel> createYouTubeList(Iterator<SearchResult> iteratorSearchResults, String query) {
 
-
-
-        Log.d("log","\n=============================================================");
-        Log.d("log",
-                "   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
-        Log.d("log","=============================================================\n");
-
         if (!iteratorSearchResults.hasNext()) {
-            Log.d("log"," There aren't any results for your query.");
+            Log.d(Constants.LOGTAG," There aren't any results for your query.");
         }
         ArrayList<YoutubeVideoModel> youtubeVideoModelArrayList = new ArrayList<>();
 
@@ -100,20 +95,13 @@ public class Search {
 
             // Double checks the kind is video.
             if (rId.getKind().equals("youtube#video")) {
-                Thumbnail thumbnail = (Thumbnail) singleVideo.getSnippet().getThumbnails().get("default");
-
-                Log.d("log"," Video Id: " + rId.getVideoId());
-                Log.d("log"," Title: " + singleVideo.getSnippet().getTitle());
-                Log.d("log"," Thumbnail: " + thumbnail.getUrl());
-                Log.d("log","\n-------------------------------------------------------------\n");
-
                 ArrayList<Object> detail = getDetail(rId.getVideoId());
 
                 youtubeVideoModelArrayList.add(new YoutubeVideoModel(
                         rId.getVideoId(),
                         singleVideo.getSnippet().getTitle(),
                         singleVideo.getSnippet().getChannelTitle(),
-                        singleVideo.getSnippet().getPublishedAt().toString(),
+                        singleVideo.getSnippet().getPublishedAt(),
                         detail.get(0).toString(),
                         detail.get(1).toString()
                 ));
@@ -136,11 +124,11 @@ public class Search {
             List<Video> videoResult = videoResponse.getItems();
             BigInteger viewCount = videoResult.get(0).getStatistics().getViewCount();
             String duration = videoResult.get(0).getContentDetails().getDuration();
-            duration.replace("PT","").replace("H",":")
+            duration = duration.replace("PT","").replace("H",":")
                     .replace("M",":").replace("S","");
 
-            detailArray.add(viewCount);
             detailArray.add(duration);
+            detailArray.add(viewCount);
 
         } catch (IOException e) {
             e.printStackTrace();
