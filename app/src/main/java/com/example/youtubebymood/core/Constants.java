8dcf237abf45cea77871afad79f85cb8b5d79677
walter <dev.ggw@gmail.com>
2020-07-11 22:56:58 +0900

    Constants 구현

--- app/src/main/java/com/example/youtubebymood/core/Constants.java
+++ app/src/main/java/com/example/youtubebymood/core/Constants.java
@@ -1,4 +1,6 @@
 package com.example.youtubebymood.core;
 
 public class Constants {
+    public static String APIKEY = "AIzaSyCsHUi0G7Iq8K6Ld9yxYAxIIIShm0J3uXE";
+    public static String LOGTAG = "tag";
 }
