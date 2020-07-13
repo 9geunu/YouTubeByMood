5d1b38d2565804ebed422b4a0f64bf0c2d7597d2
walter <dev.ggw@gmail.com>
2020-07-13 23:15:41 +0900

    WebViewActivity 수정 웹뷰에서 뒤로가기 가능하도록 설정

--- app/src/main/java/com/example/youtubebymood/WebViewActivity.java
+++ app/src/main/java/com/example/youtubebymood/WebViewActivity.java
@@ -3,6 +3,8 @@ package com.example.youtubebymood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,12 +18,30 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         webView = findViewById(R.id.web_view);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClientClass());
 
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
         String videoID = getIntent().getStringExtra("video_id");
         webView.loadUrl("https://www.youtube.com/watch?v=" + videoID);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {//뒤로가기 버튼 이벤트
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {//페이지 이동
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.d("check URL",url);
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
